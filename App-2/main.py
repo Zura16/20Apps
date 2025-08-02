e9a711308bb83ccf9768bd270726b845d16a747e
AalindCS <kaleaalind7@gmail.com>
2025-08-01 18:07:35 -0700

    Final additions

---
+++ App-2/main.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+st.header("The Best Company")
+content1 = ("Lila is introspective and warm, often described as the “human embodiment of lavender.” She journals every "
+            "night, rides a bright yellow vintage bike named “Marigold,” and loves early mornings at farmer’s "
+            "markets. She doesn’t own a TV, but she’s obsessed with old radio dramas and records her own podcast "
+            "called “The Buzzline”, where she interviews urban farmers, scientists, and sustainability advocates. "
+            "Last year, Lila spent a month in New Zealand studying Apis mellifera populations and indigenous Māori "
+            "beekeeping practices. She brought back seeds from local plants (where legal) and started a "
+            "“sister-garden” in her hometown. She also survived a surprise thunderstorm while checking on a "
+            "mountaintop hive in Oregon—an experience that she says made her feel “infinitely small and deeply "
+            "grateful.")
+
+st.write(content1)
+st.subheader("Our Team")
+
+col1, col2, col3 = st.columns(3)
+df = pandas.read_csv("data-2.csv")
+with col1:
+    for index, row in df[:4].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"]}')
+        st.write(row["role"])
+        st.image("images-2/" + row["image"])
+
+with col2:
+    for index, row in df[4:8].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"]}')
+        st.write(row["role"])
+        st.image("images-2/" + row["image"])
+
+with col3:
+    for index, row in df[8:].iterrows():
+        st.subheader(f'{row["first name"].title()} {row["last name"]}')
+        st.write(row["role"])
+        st.image("images-2/" + row["image"])
---
+++ App-2/pages/Contact_Us.py
@@ -0,0 +1,26 @@
+
+# Add the parent directory (App-2/) to the Python path
+
+
+import pandas
+import streamlit as st
+from send_email import send_email
+
+df = pandas.read_csv("topics.csv")
+
+with st.form(key="email_forms"):
+    user_email = st.text_input("Your Email Address")
+    some = st.selectbox("What topic do you want to discuss?",
+                        df["topic"])
+    raw_message = st.text_area("Text")
+    message = f'''\
+Subject: New email from {user_email}
+
+From: {user_email}
+{raw_message}
+'''
+    button = st.form_submit_button("Submit")
+
+    if button:
+        send_email(message)
+        st.info("Your email was sent successfully")
