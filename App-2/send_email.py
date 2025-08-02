dd0b71c0ef1c49c1c33464059f46b969441acfe9
AalindCS <kaleaalind7@gmail.com>
2025-08-02 15:08:12 -0700

    Final additionss

--- App-2/send_email.py
+++ App-2/send_email.py
@@ -5,7 +5,7 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "kaleaalind7@gmail.com"
-    password = "iozzepxriynlmbix"
+    password =
     receiver = "kaleaalind7@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
